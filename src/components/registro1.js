f4c05147f608eb00639d110f7c999772d7a27a27
Gabriel Josue Muñoz Robleto <gabriel22de1998@gmail.com>
2021-01-01 14:49:27 -0600

    Captura inputs del formulario

--- src/components/registro1.js
+++ src/components/registro1.js
@@ -1,63 +1,95 @@
 import React from 'react'
-import BancoDatos from './mostrador.js';
 
-function formulario(){
-    return(
-        <div className="col-5 mx-auto">
+class FormularioRegistro extends React.Component {
 
-        <form >
-            <div className="form-row">
-                <div className="form-group col-md-6">
-                <label for="inputEmail4">Email</label>
-                <input type="email" className="form-control" id="inputEmail4" placeholder="Email"/>
-                </div>
-                <div className="form-group col-md-6">
-                <label for="inputPassword4">Password</label>
-                <input type="password" className="form-control" id="inputPassword4" placeholder="Password" />
-                </div>
-            </div>
-            <div className="form-group">
-                <label for="inputAddress">Address</label>
-                <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
-            </div>
-            <div className="form-group">
-                <label for="inputAddress2">Address 2</label>
-                <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-            </div>
-            <div className="form-row">
-                <div className="form-group col-md-6">
-                <label for="inputCity">City</label>
-                <input type="text" className="form-control" id="inputCity" />
-                </div>
-                <div className="form-group col-md-4">
-                <label for="inputState">State</label>
-                <select id="inputState" className="form-control">
-                    <option selected>Choose...</option>
-                    <option>...</option>
-                </select>
-                </div>
-                <div className="form-group col-md-2">
-                <label for="inputZip">Zip</label>
-                <input type="text" className="form-control" id="inputZip" />
-                </div>
-            </div>
-            <div className="form-group">
-                <div className="form-check">
-                <input className="form-check-input" type="checkbox" id="gridCheck" />
-                <label className="form-check-label" for="gridCheck">
-                    Check me out
-                </label>
-                </div>
-            </div>
-            <button onClick={enviar} className="btn btn-primary">Sign in</button>
-        </form>
-    </div>
-    );
-
-    function enviar(e) {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            address: '',
+            estado: '',
+            ciudad: '',
+            terminos: false,
+        };
+        console.log(this.state);
+    
+        // Este enlace es necesario para hacer que `this` funcione en el callback
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+      }
+    
+      handleSubmit(e) {
+        console.log('******** Guardados');
         e.preventDefault();
-        console.log('****** Enviados')
+      }
+
+
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        (name === 'terminos')?
+            this.setState(state => ({
+                terminos: !state.terminos
+              }))
+            : this.setState({ [name]: value })
+        console.log(this.state.terminos);
+    }
+    
+      render() {
+        const { email, password, address, estado, ciudad, terminos } = this.state
+        return (
+            <div className="col-5 mx-auto">
+
+            <form onSubmit={this.handleSubmit} method='POST'>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                    <label htmlFor="inputEmail4">Email</label>
+                    <input type="email" className="form-control" id="email" name="email" value={email} 
+                onChange={this.handleChange}  placeholder="Email"/>
+                    </div>
+                    <div className="form-group col-md-6">
+                    <label htmlFor="inputPassword4">Password</label>
+                    <input type="password" className="form-control" id="password" name="password" value={password} 
+                onChange={this.handleChange} placeholder="Password" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="inputAddress">Address</label>
+                    <input type="text" className="form-control" id="address" name="address" value={address} 
+                onChange={this.handleChange} placeholder="1234 Main St" />
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                    <label htmlFor="ciudad">City</label>
+                    <input type="text" className="form-control" id="ciudad" value={ciudad} 
+                onChange={this.handleChange} name="ciudad" />
+                    </div>
+                    <div className="form-group col-md-4">
+                    <label htmlFor="estado">State</label>
+                    <select id="estado" className="form-control" name="estado"
+                            onChange={this.handleChange} >
+                        <option selected value={estado} >Choose...</option>
+                        <option value="guancaste">Guanacaste</option>
+                        <option value="alajuela">Alajuela</option>
+                        <option value="puntarena">Puntarenas</option>
+                        <option value="cartago">Cartago</option>
+                    </select>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="form-check">
+                    <input className="form-check-input" type="checkbox" id="terminos" name="terminos" 
+                        checked={terminos} onChange={this.handleChange} />
+                    <label className="form-check-label" htmlFor="haAceptadoTerminos">
+                        Check me out
+                    </label>
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary">Sign in</button>
+            </form>
+        </div>
+        );
     }
 }
 
-export default formulario;
+export default FormularioRegistro;
